6b2ecf4eda67325679b0cf60b7e675af6f4407da
dianakocsis <diana.kocsis@uniswap.org>
2025-08-23 00:03:35 -0400

    remove plan and hardcode array size

--- src/types/Plan.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-
-/// @title Plan
-/// @notice Represents a plan of actions to be executed in a pool
-struct Plan {
-    bytes actions;
-    bytes[] params;
-}
