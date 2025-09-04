3b43841eabe4f7eaf5fa7f0e6da5fab824b7e763
Alan Wu <alanwu100@gmail.com>
2025-09-04 22:22:55 +0800

    feat: remove sweep() & IMerkleClaim

--- src/distributionContracts/MerkleClaim.sol
+++ src/distributionContracts/MerkleClaim.sol
@@ -4,11 +4,10 @@ pragma solidity =0.8.17;
 import {IERC20} from "../interfaces/external/IERC20.sol";
 import {MerkleDistributorWithDeadline} from "merkle-distributor/contracts/MerkleDistributorWithDeadline.sol";
 import {IDistributionContract} from "../interfaces/IDistributionContract.sol";
-import {IMerkleClaim} from "../interfaces/IMerkleClaim.sol";
 
 /// @title MerkleClaim
 /// @notice A contract that allows users to claim tokens from a merkle distribution
-contract MerkleClaim is MerkleDistributorWithDeadline, IMerkleClaim {
+contract MerkleClaim is MerkleDistributorWithDeadline, IDistributionContract {
     constructor(address _token, bytes32 _merkleRoot, address _owner, uint256 _endTime)
         MerkleDistributorWithDeadline(_token, _merkleRoot, _endTime == 0 ? type(uint256).max : _endTime)
     {
@@ -18,16 +17,4 @@ contract MerkleClaim is MerkleDistributorWithDeadline, IMerkleClaim {
 
     /// @inheritdoc IDistributionContract
     function onTokensReceived() external {}
-
-    /// @inheritdoc IMerkleClaim
-    function sweep() external {
-        // Get the balance before withdrawal
-        uint256 balance = IERC20(token).balanceOf(address(this));
-
-        // Use the parent's withdraw function via external call
-        this.withdraw();
-
-        // Emit event with the actual amount swept
-        emit TokensSwept(owner(), balance);
-    }
 }
