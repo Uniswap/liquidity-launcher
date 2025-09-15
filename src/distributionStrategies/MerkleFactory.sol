b94a1cb803be31a012f756398441047b7591a7ef
ericneil-sanc <eric.sanchirico@uniswap.org>
2025-09-15 14:43:56 -0400

    merkle claim view function

--- src/distributionStrategies/MerkleFactory.sol
+++ src/distributionStrategies/MerkleFactory.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.17;
 import {IDistributionStrategy} from "../interfaces/IDistributionStrategy.sol";
 import {IDistributionContract} from "../interfaces/IDistributionContract.sol";
 import {MerkleClaim} from "../distributionContracts/MerkleClaim.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 
 contract MerkleClaimFactory is IDistributionStrategy {
     /// @notice Deploys a new MerkleClaim
@@ -24,5 +25,23 @@ contract MerkleClaimFactory is IDistributionStrategy {
         distributionContract = IDistributionContract(new MerkleClaim{salt: _salt}(token, merkleRoot, owner, endTime));
 
         emit DistributionInitialized(address(distributionContract), token, totalSupply);
+    }
+
+    /// @notice Get the address that a MerkleClaim contract would be deployed to
+    /// @param token The address of the ERC-20 token to distribute
+    /// @param configData ABI-encoded (merkleRoot, owner, endTime) where endTime is optional (0 = no deadline)
+    /// @param salt The salt for deterministic deployment, after hashing with msg.sender
+    /// @return merkleClaimAddress The address where the MerkleClaim would be deployed
+    function getMerkleClaimAddress(address token, bytes calldata configData, bytes32 salt)
+        external
+        view
+        returns (address merkleClaimAddress)
+    {
+        // Decode the merkle root, owner, and endTime from configData
+        (bytes32 merkleRoot, address owner, uint256 endTime) = abi.decode(configData, (bytes32, address, uint256));
+
+        bytes32 initCodeHash =
+            keccak256(abi.encodePacked(type(MerkleClaim).creationCode, abi.encode(token, merkleRoot, owner, endTime)));
+        return Create2.computeAddress(salt, initCodeHash, address(this));
     }
 }
