75ed1d26cd9f28776ccbeb5bc70c31c76bb2a99e
ens_408 <eric.sanchirico@uniswap.org>
2025-09-16 21:46:44 -0400

    OZ-L04 (#41)
    * added msg sender to salt
    
    * revert changes to lbp
    
    * only change salt in lbp test
    
    * add merkle claim view function
    
    * temporary snapshot non-isolate
    
    * resolve merge conflict: rename MerkleFactory.sol to MerkleClaimFactory.sol and update function signature
    
    * fmt

--- test/distributionStrategies/LBPStrategyBasicFactory.t.sol
+++ test/distributionStrategies/LBPStrategyBasicFactory.t.sol
@@ -115,7 +115,7 @@ contract LBPStrategyBasicFactoryTest is Test {
     }
 
     function test_getLBPAddress_succeeds() public {
-        bytes32 salt = 0x7fa9385be102ac3eac297483dd6233d62b3e1496c857faf801c8174cae36c06f;
+        bytes32 salt = 0x00000000000000000000000000000000000000000000000000000000000011cf;
         address lbpAddress = factory.getLBPAddress(
             address(token),
             TOTAL_SUPPLY,
