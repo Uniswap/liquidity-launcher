de527c007bbc783b4b199fd929f76175c8755c9d
Eric Zhong <eric.zhong@uniswap.org>
2026-01-15 11:21:43 -0500

    feat: add feeTapper (#114)
    * feat: implement linked list, kegs, and fix release logic
    
    * fix up and add gas tests
    
    * fix: restrict release rate to valid values
    
    * fix: add tests for head/tail reset
    
    * fix: add amoutn too large check
    
    * chore: natspec

--- src/periphery/FeeTapper.sol
+++ src/periphery/FeeTapper.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.8.26;
 
 import {Currency, CurrencyLibrary} from "@uniswap/v4-core/src/types/Currency.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 import {FixedPointMathLib} from "solady/utils/FixedPointMathLib.sol";
-import {Tap, IFeeTapper} from "../interfaces/periphery/IFeeTapper.sol";
+import {Tap, Keg, IFeeTapper} from "../interfaces/periphery/IFeeTapper.sol";
 
 /// @title FeeTapper
 /// @notice Singleton contract which handles the streaming of incoming protocol fees to TokenJar
@@ -14,13 +13,22 @@ contract FeeTapper is IFeeTapper, Ownable {
 
     address public immutable tokenJar;
 
+    /// @notice Mapping of currencies to Taps
     mapping(Currency => Tap) private $_taps;
+    /// @notice Linked list of kegs. Taps manage the head and tail of their respective kegs.
+    mapping(uint32 => Keg) private $_kegs;
+    /// @notice The id of the next keg to be created
+    uint32 public nextId;
 
+    /// @notice Basis points denominator
     uint24 public constant BPS = 10_000;
     /// @notice The release rate for accrued protocol fees in basis points per block.
     ///         For example, at 10 basis points the full amount is released over 1_000 blocks.
     /// @dev    This helps smooth out the release of fees which is useful for integrating with TokenJar fee exchangers.
     uint24 public perBlockReleaseRate = 10;
+
+    /// @notice The maximum supported balance to prevent overflowing a uint128
+    uint128 public constant MAX_BALANCE = type(uint128).max / BPS;
 
     constructor(address _tokenJar, address _owner) Ownable(_owner) {
         tokenJar = _tokenJar;
@@ -31,58 +39,136 @@ contract FeeTapper is IFeeTapper, Ownable {
         return $_taps[currency];
     }
 
-    /// @notice Sets the release rate for accrued protocol fees in basis points per block
-    /// @dev Only callable by the owner
-    /// @param _perBlockReleaseRate The new release rate in basis points per block. Must be non zero and <= BPS
+    /// @notice Gets the keg for the given id
+    function kegs(uint32 id) external view returns (Keg memory) {
+        return $_kegs[id];
+    }
+
+    /// @inheritdoc IFeeTapper
     function setReleaseRate(uint24 _perBlockReleaseRate) external onlyOwner {
-        if (_perBlockReleaseRate == 0 || _perBlockReleaseRate > BPS) revert InvalidReleaseRate();
+        if (_perBlockReleaseRate == 0 || _perBlockReleaseRate > BPS) revert ReleaseRateOutOfBounds();
+        if (BPS % _perBlockReleaseRate != 0) revert InvalidReleaseRate();
         perBlockReleaseRate = _perBlockReleaseRate;
         emit ReleaseRateSet(_perBlockReleaseRate);
     }
 
-    /// @notice Syncs the fee tapper with received protocol fees. Callable by anyone
-    /// @param currency The currency to sync
+    /// @inheritdoc IFeeTapper
     function sync(Currency currency) external {
         // Release any accrued protocol fees
         _release(currency);
 
         Tap storage $tap = $_taps[currency];
+        // Silently truncates any received balances over uint128.max
         uint128 balance = uint128(currency.balanceOfSelf());
+        uint128 oldBalance = $tap.balance;
         // noop if there hasn't been a change in balance
-        if (balance == $tap.balance) return;
+        if (balance == oldBalance) return;
 
-        $tap.lastReleaseBlock = uint64(block.number);
+        unchecked {
+            nextId++;
+        }
+        uint32 next = nextId;
+
+        uint48 endBlock = uint48(block.number + BPS / perBlockReleaseRate);
+        uint128 amount = balance - oldBalance;
+        // Revert if the amount added to the tap would eventually overflow a uint128
+        if (amount > MAX_BALANCE) revert AmountTooLarge();
+        uint128 perBlockReleaseAmount = amount * perBlockReleaseRate;
+
+        Keg storage $keg = $_kegs[next];
+        $keg.perBlockReleaseAmount += perBlockReleaseAmount;
+        $keg.lastReleaseBlock = uint48(block.number);
+        $keg.endBlock = endBlock;
+        if ($tap.head == 0) {
+            $tap.head = next;
+            $tap.tail = next;
+        } else {
+            $_kegs[$tap.tail].next = next;
+            $keg.next = 0;
+            $tap.tail = next;
+        }
         $tap.balance = balance;
 
+        emit Deposited(next, Currency.unwrap(currency), amount, endBlock);
         emit Synced(Currency.unwrap(currency), balance);
     }
 
-    /// @notice Releases any accrued protocol fees to the protocol fee recipient
-    /// @dev Callable by anyone to release any accrued protocol fees
-    function release(Currency currency) external returns (uint192) {
-        return _release(currency);
+    /// @inheritdoc IFeeTapper
+    function release(Currency currency) external returns (uint128) {
+        return _process(currency, _release(currency));
     }
 
-    /// @notice Releases currency to the protocol fee recipient over time according to the release rate
-    function _release(Currency currency) internal returns (uint192) {
+    /// @inheritdoc IFeeTapper
+    function release(Currency currency, uint32 id) external returns (uint128) {
+        // Require id to exist in Tap
+        uint32 next = $_taps[currency].head;
+        while (next != 0) {
+            if (next == id) {
+                break;
+            }
+            next = $_kegs[next].next;
+        }
+        if (next == 0) revert KegNotFound(id);
+        return _process(currency, _releaseKeg($_kegs[id], id));
+    }
+
+    /// @notice Releases a single keg for a given currency
+    /// @param id The id of the keg to release
+    function _releaseKeg(Keg memory keg, uint32 id) internal returns (uint128 releasedAmount) {
+        releasedAmount = uint128(
+            keg.perBlockReleaseAmount * (FixedPointMathLib.min(block.number, keg.endBlock) - keg.lastReleaseBlock)
+        );
+        if (keg.lastReleaseBlock != block.number) {
+            $_kegs[id].lastReleaseBlock = uint48(block.number);
+        }
+        return releasedAmount;
+    }
+
+    /// @notice Releases all kegs for a given currency
+    function _release(Currency currency) internal returns (uint128 releasedAmount) {
         Tap storage $tap = $_taps[currency];
         if ($tap.balance == 0) {
-            $tap.lastReleaseBlock = uint64(block.number);
             return 0;
         }
-        uint256 elapsed = block.number - $tap.lastReleaseBlock;
-        if (elapsed == 0) return 0;
 
-        // Calculate the amount of protocol fees to release
-        uint192 toRelease = uint192(FixedPointMathLib.fullMulDiv($tap.balance * perBlockReleaseRate, elapsed, BPS));
-        if (toRelease > $tap.balance) toRelease = $tap.balance;
+        uint32 next = $tap.head;
+        uint32 newHead;
+        // Itereate through all kegs. This can be very costly if there are lot of kegs.
+        while (next != 0) {
+            Keg memory keg = $_kegs[next];
+            uint32 curr = next;
+            releasedAmount += _releaseKeg(keg, curr);
+            next = keg.next;
+            if (keg.endBlock <= block.number) {
+                // Update the head (since this keg is fully released)
+                newHead = next;
+                // Delete the old keg
+                delete $_kegs[curr];
+                // If we have iterated through all of the kegs reset the head/tail to 0
+                if (next == 0) {
+                    $tap.head = 0;
+                    $tap.tail = 0;
+                    break;
+                }
+            }
+        }
+        // Update the head if it needed
+        if (newHead != 0) {
+            $tap.head = newHead;
+        }
+        return releasedAmount;
+    }
 
-        $tap.balance -= toRelease;
-        $tap.lastReleaseBlock = uint64(block.number);
+    /// @notice Transfers the released amount to the token jar
+    function _process(Currency _currency, uint128 _releasedAmount) internal returns (uint128) {
+        // Because we deferred dividing by BPS when storing the perBlockReleaseAmount, we need to divide now
+        uint128 toRelease = _releasedAmount / BPS;
+        // Update the tap balance
+        $_taps[_currency].balance -= toRelease;
 
         if (toRelease > 0) {
-            currency.transfer(tokenJar, toRelease);
-            emit Released(Currency.unwrap(currency), toRelease);
+            _currency.transfer(tokenJar, toRelease);
+            emit Released(Currency.unwrap(_currency), toRelease);
         }
         return toRelease;
     }
--- test/periphery/FeeTapper.t.sol
+++ test/periphery/FeeTapper.t.sol
@@ -50,27 +50,39 @@ contract FeeTapperTest is Test {
     }
 
     function test_setReleaseRate_WhenEqZero() public {
-        // it reverts with {InvalidReleaseRate()}
+        // it reverts with {ReleaseRateOutOfBounds()}
 
         vm.prank(owner);
-        vm.expectRevert(IFeeTapper.InvalidReleaseRate.selector);
+        vm.expectRevert(IFeeTapper.ReleaseRateOutOfBounds.selector);
         feeTapper.setReleaseRate(0);
     }
 
     function test_setReleaseRate_WhenGtBPS(uint24 _perBlockReleaseRate) public {
-        // it reverts with {InvalidReleaseRate()}
+        // it reverts with {ReleaseRateOutOfBounds()}
 
         _perBlockReleaseRate = uint24(bound(_perBlockReleaseRate, BPS + 1, type(uint24).max));
+
+        vm.prank(owner);
+        vm.expectRevert(IFeeTapper.ReleaseRateOutOfBounds.selector);
+        feeTapper.setReleaseRate(_perBlockReleaseRate);
+    }
+
+    function test_setReleaseRate_WhenNotDivisibleByBPS(uint24 _perBlockReleaseRate) public {
+        // it reverts with {InvalidReleaseRate()}
+
+        _perBlockReleaseRate = uint24(bound(_perBlockReleaseRate, 1, BPS - 1));
+        vm.assume(BPS % _perBlockReleaseRate != 0);
 
         vm.prank(owner);
         vm.expectRevert(IFeeTapper.InvalidReleaseRate.selector);
         feeTapper.setReleaseRate(_perBlockReleaseRate);
     }
 
-    function test_setReleaseRate_WhenLTEBPS(uint24 _perBlockReleaseRate) public {
+    function test_setReleaseRate_WhenLTEBPSAndDivisibleByBPS(uint24 _perBlockReleaseRate) public {
         // it succeeds
 
         _perBlockReleaseRate = uint24(bound(_perBlockReleaseRate, 1, BPS));
+        vm.assume(BPS % _perBlockReleaseRate == 0);
 
         vm.prank(owner);
         vm.expectEmit(true, true, true, true);
@@ -79,10 +91,32 @@ contract FeeTapperTest is Test {
         assertEq(feeTapper.perBlockReleaseRate(), _perBlockReleaseRate);
     }
 
+    /// forge-config: default.isolate = true
+    /// forge-config: ci.isolate = true
+    function test_sync_gas() public {
+        Currency currency = Currency.wrap(address(erc20Currency));
+        deal(address(erc20Currency), address(feeTapper), 1e18);
+        feeTapper.sync(currency);
+        vm.snapshotGasLastCall("sync_newTap");
+
+        deal(address(erc20Currency), address(feeTapper), 1e18);
+        feeTapper.sync(currency);
+        vm.snapshotGasLastCall("sync_existingTap");
+    }
+
+    function test_sync_WhenAmountIsTooLarge() public {
+        // it reverts with {AmountTooLarge()}
+
+        Currency currency = Currency.wrap(address(erc20Currency));
+        deal(address(erc20Currency), address(feeTapper), feeTapper.MAX_BALANCE() + 1);
+        vm.expectRevert(IFeeTapper.AmountTooLarge.selector);
+        feeTapper.sync(currency);
+    }
+
     function test_sync_WhenCurrencyIsNotAddressZero(uint128 _feeAmount) public {
         // it emits a {Synced()} event
 
-        _feeAmount = uint128(bound(_feeAmount, 1, type(uint128).max / feeTapper.perBlockReleaseRate()));
+        _feeAmount = uint128(bound(_feeAmount, 1, feeTapper.MAX_BALANCE()));
 
         Currency currency = Currency.wrap(address(erc20Currency));
 
@@ -100,9 +134,9 @@ contract FeeTapperTest is Test {
         uint64 _elapsed,
         bool _useNativeCurrency
     ) public {
-        // it adds fee amount to the tap amount
-        // it updates the release rate
-        // it updates the last release block
+        // it adds fee amount to the tap balance
+        // it creates a new keg
+        // it emits a {Deposited()} event
         // it emits a {Synced()} event
 
         // Low bounds to avoid overflows later on
@@ -113,12 +147,10 @@ contract FeeTapperTest is Test {
         _deal(address(feeTapper), _feeAmount, _useNativeCurrency);
         feeTapper.sync(currency);
         assertEq(feeTapper.taps(currency).balance, _feeAmount);
-        assertEq(feeTapper.taps(currency).lastReleaseBlock, uint64(block.number));
 
         _deal(address(feeTapper), _additionalFeeAmount, _useNativeCurrency);
         feeTapper.sync(currency);
         assertEq(feeTapper.taps(currency).balance, _feeAmount + _additionalFeeAmount);
-        assertEq(feeTapper.taps(currency).lastReleaseBlock, uint64(block.number));
 
         _elapsed = uint64(bound(_elapsed, 1, BPS / feeTapper.perBlockReleaseRate()));
 
@@ -137,62 +169,263 @@ contract FeeTapperTest is Test {
     function test_release_WhenElapsedIsZero(uint128 _feeAmount) public {
         // it returns 0
 
-        _feeAmount = uint128(bound(_feeAmount, 1, type(uint128).max / feeTapper.perBlockReleaseRate()));
+        _feeAmount = uint128(bound(_feeAmount, 1, feeTapper.MAX_BALANCE()));
 
         Currency currency = Currency.wrap(address(0));
         _deal(address(feeTapper), _feeAmount, true);
         feeTapper.sync(currency);
 
         assertEq(feeTapper.taps(currency).balance, _feeAmount);
-        assertEq(feeTapper.taps(currency).lastReleaseBlock, uint64(block.number));
 
         uint256 amount = feeTapper.release(currency);
         assertEq(amount, 0);
     }
 
-    function test_release_WhenToReleaseIsGreaterThanTapAmount(uint128 _feeAmount) public {
+    function test_release_WhenKegIdIsZero(Currency currency) public {
+        // it reverts with {KegNotFound(0)}
+
+        vm.expectRevert(abi.encodeWithSelector(IFeeTapper.KegNotFound.selector, 0));
+        feeTapper.release(currency, 0);
+    }
+
+    function test_release_WhenKegIdIsZeroAndTapIsNotEmpty() public {
+        // it reverts with {KegNotFound(0)}
+
+        _deal(address(feeTapper), 1e18, true);
+        Currency currency = Currency.wrap(address(0));
+        feeTapper.sync(currency);
+
+        vm.expectRevert(abi.encodeWithSelector(IFeeTapper.KegNotFound.selector, 0));
+        feeTapper.release(currency, 0);
+
+        // for different currency
+        Currency otherCurrency = Currency.wrap(address(1));
+        vm.expectRevert(abi.encodeWithSelector(IFeeTapper.KegNotFound.selector, 0));
+        feeTapper.release(otherCurrency, 0);
+    }
+
+    function test_release_WhenKegIsNotFound(uint32 _id) public {
+        // it reverts with {KegNotFound(_id)}
+
+        _id = uint32(bound(_id, 1, type(uint32).max));
+
+        vm.expectRevert(abi.encodeWithSelector(IFeeTapper.KegNotFound.selector, _id));
+        feeTapper.release(Currency.wrap(address(0)), _id);
+    }
+
+    function test_release_WhenKegIdIsNotInTap() public {
+        // it reverts with {KegNotFound(_id)}
+
+        Currency currency = Currency.wrap(address(0));
+        _deal(address(feeTapper), 1e18, true);
+        feeTapper.sync(currency);
+
+        // id 1 is for address(0)
+        vm.expectRevert(abi.encodeWithSelector(IFeeTapper.KegNotFound.selector, 1));
+        Currency otherCurrency = Currency.wrap(address(1));
+        feeTapper.release(otherCurrency, 1);
+    }
+
+    /// forge-config: default.isolate = true
+    /// forge-config: ci.isolate = true
+    function test_release_nativeCurrency_single_gas() public {
+        Currency currency = Currency.wrap(address(0));
+        _deal(address(feeTapper), 1e18, true);
+        feeTapper.sync(currency);
+
+        vm.roll(block.number + 1);
+        feeTapper.release(currency);
+        vm.snapshotGasLastCall("release_nativeCurrency_single");
+
+        vm.roll(block.number + BPS);
+        feeTapper.release(currency);
+        vm.snapshotGasLastCall("release_nativeCurrency_single_deletion");
+    }
+
+    /// forge-config: default.isolate = true
+    /// forge-config: ci.isolate = true
+    function test_release_keg_nativeCurrency_gas() public {
+        Currency currency = Currency.wrap(address(0));
+        _deal(address(feeTapper), 1e18, true);
+        feeTapper.sync(currency);
+
+        vm.roll(block.number + 1);
+        feeTapper.release(currency, 1);
+        vm.snapshotGasLastCall("release_keg_nativeCurrency_single");
+
+        vm.roll(block.number + BPS);
+        feeTapper.release(currency, 1);
+        vm.snapshotGasLastCall("release_keg_nativeCurrency_single_deletion");
+    }
+
+    /// forge-config: default.isolate = true
+    /// forge-config: ci.isolate = true
+    function test_release_erc20Currency_single_gas() public {
+        Currency currency = Currency.wrap(address(erc20Currency));
+        _deal(address(feeTapper), 1e18, false);
+        feeTapper.sync(currency);
+
+        vm.roll(block.number + 1);
+        feeTapper.release(currency);
+        vm.snapshotGasLastCall("release_erc20Currency_single");
+
+        vm.roll(block.number + BPS);
+        feeTapper.release(currency);
+        vm.snapshotGasLastCall("release_erc20Currency_single_deletion");
+    }
+
+    /// forge-config: default.isolate = true
+    /// forge-config: ci.isolate = true
+    function test_release_keg_erc20Currency_gas() public {
+        Currency currency = Currency.wrap(address(erc20Currency));
+        _deal(address(feeTapper), 1e18, false);
+        feeTapper.sync(currency);
+
+        vm.roll(block.number + 1);
+        feeTapper.release(currency, 1);
+        vm.snapshotGasLastCall("release_keg_erc20Currency_single");
+
+        vm.roll(block.number + BPS);
+        feeTapper.release(currency, 1);
+        vm.snapshotGasLastCall("release_keg_erc20Currency_single_deletion");
+    }
+
+    /// forge-config: default.isolate = true
+    /// forge-config: ci.isolate = true
+    function test_release_nativeCurrency_multiple_gas() public {
+        Currency currency = Currency.wrap(address(0));
+        for (uint64 i = 0; i < 3; i++) {
+            _deal(address(feeTapper), 1e18, true);
+            feeTapper.sync(currency);
+        }
+
+        vm.roll(block.number + 1);
+        feeTapper.release(currency);
+        vm.snapshotGasLastCall("release_nativeCurrency_multiple");
+
+        vm.roll(block.number + BPS);
+        feeTapper.release(currency);
+        vm.snapshotGasLastCall("release_nativeCurrency_multiple_deletion");
+    }
+
+    function test_release_WhenToReleaseIsGreaterThanTapAmount(uint128 _feeAmount, uint64 _elapsed) public {
         // it returns the rest of the tap amount
 
-        _feeAmount = uint128(bound(_feeAmount, 1, type(uint128).max / feeTapper.perBlockReleaseRate()));
+        _elapsed = uint64(bound(_elapsed, BPS / feeTapper.perBlockReleaseRate(), type(uint64).max));
+        _feeAmount = uint128(bound(_feeAmount, 1, feeTapper.MAX_BALANCE()));
 
         Currency currency = Currency.wrap(address(0));
         _deal(address(feeTapper), _feeAmount, true);
         feeTapper.sync(currency);
 
         // ensure that there is more to release than the tap amount
-        vm.roll(block.number + BPS + 1);
+        vm.roll(block.number + _elapsed);
 
         uint256 amount = feeTapper.release(currency);
         assertEq(amount, _feeAmount);
         assertEq(feeTapper.taps(currency).balance, 0);
-        assertEq(feeTapper.taps(currency).lastReleaseBlock, uint64(block.number));
     }
 
     function test_release_WhenToReleaseIsLTEThanTapAmount(uint128 _feeAmount, uint64 _elapsed, bool _useNativeCurrency)
         public
     {
         // it updates the tap amount
-        // it updates the lastReleaseBlock
         // it emits a {Released()} event
 
         _elapsed = uint64(bound(_elapsed, 1, BPS / feeTapper.perBlockReleaseRate()));
-        _feeAmount = uint128(bound(_feeAmount, 1, type(uint128).max / feeTapper.perBlockReleaseRate() / _elapsed));
+        _feeAmount = uint128(bound(_feeAmount, 1, feeTapper.MAX_BALANCE()));
 
         Currency currency = _useNativeCurrency ? Currency.wrap(address(0)) : Currency.wrap(address(erc20Currency));
         _deal(address(feeTapper), _feeAmount, _useNativeCurrency);
         feeTapper.sync(currency);
 
         assertEq(feeTapper.taps(currency).balance, _feeAmount);
-        assertEq(feeTapper.taps(currency).lastReleaseBlock, uint64(block.number));
 
         vm.roll(block.number + _elapsed);
         vm.expectEmit(true, true, true, true);
-        uint192 expectedReleased = (_feeAmount * feeTapper.perBlockReleaseRate() * _elapsed) / BPS;
+        uint128 expectedReleased = (_feeAmount * feeTapper.perBlockReleaseRate() * _elapsed) / BPS;
         emit IFeeTapper.Released(Currency.unwrap(currency), expectedReleased);
         vm.assume(expectedReleased > 0);
-        uint192 released = feeTapper.release(currency);
+        uint128 released = feeTapper.release(currency);
         assertEq(released, expectedReleased);
         assertEq(feeTapper.taps(currency).balance, _feeAmount - released);
-        assertEq(feeTapper.taps(currency).lastReleaseBlock, uint64(block.number));
+    }
+
+    function test_release_IsLinear(uint128 _feeAmount, bool _useNativeCurrency) public {
+        // it releases the amount of protocol fees based on the release rate
+
+        _feeAmount = uint128(bound(_feeAmount, 1, feeTapper.MAX_BALANCE()));
+
+        Currency currency = _useNativeCurrency ? Currency.wrap(address(0)) : Currency.wrap(address(erc20Currency));
+        _deal(address(feeTapper), _feeAmount, _useNativeCurrency);
+        feeTapper.sync(currency);
+
+        uint256 snapshotId = vm.snapshot();
+
+        // Maximize number of releases by calling one per block
+        uint128 totalReleased = 0;
+        uint256 maxDust = 0;
+        for (uint64 i = 0; i < BPS / feeTapper.perBlockReleaseRate(); i++) {
+            vm.roll(block.number + 1);
+            totalReleased += feeTapper.release(currency);
+            maxDust++;
+        }
+
+        vm.revertTo(snapshotId);
+
+        // Now test releasing all in one go after BPS / perBlockReleaseRate() blocks
+        vm.roll(block.number + BPS / feeTapper.perBlockReleaseRate());
+        uint256 endTotalReleased = feeTapper.release(currency);
+        assertApproxEqAbs(endTotalReleased, totalReleased, maxDust, "total released should be the same");
+    }
+
+    function test_release_WhenEmptyKegsAreReleased(
+        uint128 _feeAmount,
+        uint128 _additionalFeeAmount,
+        bool _useNativeCurrency
+    ) public {
+        // it does not delete the keg when fully released
+        // it does not update the head/tail of the tap
+        // after adding a new keg, the old keg is deleted
+        // after releasing the new keg, it becomes the head/tail of the tap
+
+        _feeAmount = uint128(bound(_feeAmount, 1, feeTapper.MAX_BALANCE()));
+        _additionalFeeAmount = uint128(bound(_additionalFeeAmount, 1, feeTapper.MAX_BALANCE()));
+
+        Currency currency = _useNativeCurrency ? Currency.wrap(address(0)) : Currency.wrap(address(erc20Currency));
+        _deal(address(feeTapper), _feeAmount, _useNativeCurrency);
+        feeTapper.sync(currency);
+
+        uint48 endBlock = uint48(block.number + BPS / feeTapper.perBlockReleaseRate());
+
+        vm.roll(block.number + BPS / feeTapper.perBlockReleaseRate());
+        uint128 released = feeTapper.release(currency, 1);
+        assertEq(released, _feeAmount);
+        assertEq(feeTapper.taps(currency).balance, 0);
+
+        // assert that the keg is not deleted
+        assertEq(feeTapper.taps(currency).head, 1);
+        assertEq(feeTapper.taps(currency).tail, 1);
+        assertEq(feeTapper.kegs(1).perBlockReleaseAmount, _feeAmount * feeTapper.perBlockReleaseRate());
+        assertEq(feeTapper.kegs(1).endBlock, endBlock);
+
+        // make another deposit
+        _deal(address(feeTapper), _additionalFeeAmount, _useNativeCurrency);
+        feeTapper.sync(currency);
+
+        vm.roll(block.number + 1);
+        released = feeTapper.release(currency);
+
+        // assert that the old keg is deleted and the new keg is the head/tail
+        assertEq(feeTapper.taps(currency).head, 2);
+        assertEq(feeTapper.taps(currency).tail, 2);
+        assertEq(feeTapper.kegs(1).perBlockReleaseAmount, 0);
+        assertEq(feeTapper.kegs(1).endBlock, 0);
+
+        // assert that after the full release, the head/tail are reset
+        vm.roll(block.number + BPS / feeTapper.perBlockReleaseRate());
+        released = feeTapper.release(currency);
+        assertEq(feeTapper.taps(currency).head, 0);
+        assertEq(feeTapper.taps(currency).tail, 0);
     }
 }
