a50af7b5cc2c79863587f4797d4478c1612605d1
Eric Zhong <eric.zhong@uniswap.org>
2026-01-29 15:28:05 -0500

    chore: add governed lbp strategy factory (#126)

--- script/contracts/DeployAll.s.sol
+++ script/contracts/DeployAll.s.sol
@@ -3,19 +3,22 @@ pragma solidity 0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 import {DeployLiquidityLauncherScript} from "./DeployLiquidityLauncher.s.sol";
-import {DeployAdvancedLBPStrategyFactoryScript} from "./DeployAdvancedLBPStrategyFactory.sol";
+import {DeployAdvancedLBPStrategyFactoryScript} from "./DeployAdvancedLBPStrategyFactory.s.sol";
 import {DeployFullRangeLBPStrategyFactoryScript} from "./DeployFullRangeLBPStrategyFactory.s.sol";
+import {DeployGovernedLBPStrategyFactoryScript} from "./DeployGovernedLBPStrategyFactory.s.sol";
 import {console} from "forge-std/console.sol";
 
 contract DeployAllScript is Script {
     DeployLiquidityLauncherScript public liquidityLauncherDeployer;
     DeployAdvancedLBPStrategyFactoryScript public advancedLBPStrategyFactoryDeployer;
     DeployFullRangeLBPStrategyFactoryScript public fullRangeLBPStrategyFactoryDeployer;
+    DeployGovernedLBPStrategyFactoryScript public governedLBPStrategyFactoryDeployer;
 
     constructor() {
         liquidityLauncherDeployer = new DeployLiquidityLauncherScript();
         advancedLBPStrategyFactoryDeployer = new DeployAdvancedLBPStrategyFactoryScript();
         fullRangeLBPStrategyFactoryDeployer = new DeployFullRangeLBPStrategyFactoryScript();
+        governedLBPStrategyFactoryDeployer = new DeployGovernedLBPStrategyFactoryScript();
     }
 
     function run() public {
@@ -24,5 +27,6 @@ contract DeployAllScript is Script {
         liquidityLauncherDeployer.run();
         advancedLBPStrategyFactoryDeployer.run();
         fullRangeLBPStrategyFactoryDeployer.run();
+        governedLBPStrategyFactoryDeployer.run();
     }
 }
