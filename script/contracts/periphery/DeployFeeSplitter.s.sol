be73d321ffd59250663977366054110b85a3d198
Eric Zhong <eric.zhong@uniswap.org>
2026-07-29 09:43:14 -0400

    chore: add delpoy scripts

--- script/contracts/periphery/DeployFeeSplitter.s.sol
+++ script/contracts/periphery/DeployFeeSplitter.s.sol
@@ -11,20 +11,13 @@ import {DeployParameters, Parameters} from "../Parameters.sol";
 /// @title DeployFeeSplitterScript
 /// @notice Deploys a FeeSplitter contract for the given chain
 contract DeployFeeSplitterScript is Script, Parameters {
-    function run() public returns (address feeSplitter) {
-        DeployParameters memory params = getParameters(block.chainid);
-
-        // Simple fee split setup.
-        address beneficiaryVault = vm.envAddress("BENEFICIARY_VAULT");
-        address compoundingClaimRecipient = vm.envAddress("COMPOUNDING_CLAIM_RECIPIENT");
-        if (beneficiaryVault == address(0)) revert("env: BENEFICIARY_VAULT not set");
-        if (compoundingClaimRecipient == address(0)) revert("env: COMPOUNDING_CLAIM_RECIPIENT not set");
-
-        FeeSplit[] memory feeSplits = new FeeSplit[](2);
-        feeSplits[0] = FeeSplit({recipient: beneficiaryVault, nativeBps: 4_000, tokenBps: 0, useCallback: true}); // 40% of ETH fees go to beneficiary vault
-        feeSplits[1] =
-            FeeSplit({recipient: compoundingClaimRecipient, nativeBps: 6_000, tokenBps: 10_000, useCallback: true}); // Remainder of ETH and all token fees go to compounder
-
+    /// @notice Deploys a FeeSplitter contract with the given parameters
+    /// @param params The parameters for the FeeSplitter
+    /// @return feeSplitter The address of the deployed FeeSplitter
+    function _deploy(DeployParameters memory params, FeeSplit[] memory feeSplits)
+        internal
+        returns (address feeSplitter)
+    {
         // Optionally use a salt for deployment
         bytes32 salt = bytes32(0);
         bytes memory bytecode =
@@ -40,6 +33,36 @@ contract DeployFeeSplitterScript is Script, Parameters {
         feeSplitter = Create2.deploy(0, salt, bytecode);
 
         console.log("FeeSplitter deployed to:", feeSplitter);
-        return feeSplitter;
+    }
+
+    function deployWithCreatorFee() public returns (address feeSplitter) {
+        DeployParameters memory params = getParameters(block.chainid);
+
+        // Simple fee split setup.
+        address beneficiaryVault = vm.envAddress("BENEFICIARY_VAULT");
+        address compoundingClaimRecipient = vm.envAddress("COMPOUNDING_CLAIM_RECIPIENT");
+        if (beneficiaryVault == address(0)) revert("env: BENEFICIARY_VAULT not set");
+        if (compoundingClaimRecipient == address(0)) revert("env: COMPOUNDING_CLAIM_RECIPIENT not set");
+
+        FeeSplit[] memory feeSplits = new FeeSplit[](2);
+        feeSplits[0] = FeeSplit({recipient: beneficiaryVault, nativeBps: 4_000, tokenBps: 0, useCallback: true}); // 40% of ETH fees go to beneficiary vault
+        feeSplits[1] =
+            FeeSplit({recipient: compoundingClaimRecipient, nativeBps: 6_000, tokenBps: 10_000, useCallback: true}); // Remainder of ETH and all token fees go to compounder
+
+        return _deploy(params, feeSplits);
+    }
+
+    function deployWithoutCreatorFee() public returns (address feeSplitter) {
+        DeployParameters memory params = getParameters(block.chainid);
+
+        // Simple fee split setup.
+        address compoundingClaimRecipient = vm.envAddress("COMPOUNDING_CLAIM_RECIPIENT");
+        if (compoundingClaimRecipient == address(0)) revert("env: COMPOUNDING_CLAIM_RECIPIENT not set");
+
+        FeeSplit[] memory feeSplits = new FeeSplit[](1);
+        feeSplits[0] =
+            FeeSplit({recipient: compoundingClaimRecipient, nativeBps: 10_000, tokenBps: 10_000, useCallback: true}); // 100% of ETH and token fees go to compounder
+
+        return _deploy(params, feeSplits);
     }
 }
