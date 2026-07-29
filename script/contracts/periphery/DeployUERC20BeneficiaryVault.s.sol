b9e15ac148d3cddfb40f72fd20cadd5b3329148c
Eric Zhong <eric.zhong@uniswap.org>
2026-07-29 11:56:31 -0400

    chore: add RH periphery deploy sections, fix script (#216)
    * chore: add RH periphery deploy sections, fix script
    
    * chore: update InstantLaunchDeploy

--- script/contracts/periphery/DeployUERC20BeneficiaryVault.s.sol
+++ script/contracts/periphery/DeployUERC20BeneficiaryVault.s.sol
@@ -3,39 +3,39 @@ pragma solidity 0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 import {DeployParameters, Parameters} from "../Parameters.sol";
-import {BeneficiaryVault} from "../../../src/periphery/BeneficiaryVault.sol";
+import {UERC20BeneficiaryVault} from "../../../src/periphery/UERC20BeneficiaryVault.sol";
 import {console} from "forge-std/console.sol";
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
 
-/// @title DeployBeneficiaryVaultScript
-contract DeployBeneficiaryVaultScript is Script, Parameters {
-    function run() public returns (address beneficiaryVault) {
+/// @title DeployUERC20BeneficiaryVaultScript
+contract DeployUERC20BeneficiaryVaultScript is Script, Parameters {
+    function run() public returns (address uerc20BeneficiaryVault) {
         DeployParameters memory params = getParameters(block.chainid);
 
         // Optionally use a salt for deployment
         bytes32 salt = bytes32(0);
         bytes32 initCodeHash = keccak256(
             abi.encodePacked(
-                type(BeneficiaryVault).creationCode,
+                type(UERC20BeneficiaryVault).creationCode,
                 abi.encode(params.positionManager, getTokenJar(block.chainid), DEFAULT_BURN_ADDRESS)
             )
         );
         address expectedAddress = Create2.computeAddress(salt, initCodeHash, DEFAULT_CREATE2_DEPLOYER);
         if (expectedAddress.code.length > 0) {
-            console.log("Skipping deployment of BeneficiaryVault as it already exists at", expectedAddress);
+            console.log("Skipping deployment of UERC20BeneficiaryVault as it already exists at", expectedAddress);
             return expectedAddress;
         }
 
         vm.broadcast();
-        beneficiaryVault = Create2.deploy(
+        uerc20BeneficiaryVault = Create2.deploy(
             0,
             salt,
             abi.encodePacked(
-                type(BeneficiaryVault).creationCode,
+                type(UERC20BeneficiaryVault).creationCode,
                 abi.encode(params.positionManager, getTokenJar(block.chainid), DEFAULT_BURN_ADDRESS)
             )
         );
-        console.log("BeneficiaryVault deployed to:", beneficiaryVault);
-        return beneficiaryVault;
+        console.log("UERC20BeneficiaryVault deployed to:", uerc20BeneficiaryVault);
+        return uerc20BeneficiaryVault;
     }
 }
